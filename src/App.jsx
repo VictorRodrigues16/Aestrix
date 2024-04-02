3fe9392135fe2edd5efd993799269424facfd33f
Victor rodrigues <vitorr.29@hotmail.com>
2024-04-02 14:42:00 -0300

    React-model

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-
+import {} from 'react'
+import Footer from "./components/Footer"
+import Nav from "./components/Nav"
+import { Outlet } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      
+      <Nav/>
+      <Outlet/>
+      <Footer/>
     </>
   )
 }
