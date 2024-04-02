92ea88e797e76012a9ffc72463533baadf111a7e
Lucas Reis Diniz <lucasreis.dnz@gmail.com>
2024-04-02 20:27:03 -0300

    feat: add nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,30 @@
+import '../styles/Nav.scss'
+import { Link } from 'react-router-dom'
+import logo from "../assets/logo-nav.svg"
+import storeCart from '../assets/store-cart-icon.svg'
+
+import Sobre from '../routes/Sobre'
 
 export default function Nav() {
 
     return (
       <>
-        
+        <header>
+          <div className='nav-left-container'>
+            <div className="nav-left-content">
+              <img src={logo} id="logo-nav" />
+            </div>
+          <nav className="nav-center-content-links">
+            <Link to='/'> Home </Link>
+            <Link to='/sobre'>Sobre</Link>
+            <Link to='/produtos'>Produtos</Link>
+            <Link to='/contato'>Contato</Link>
+          </nav>
+          </div>
+          <div className="nav-store-cart">
+            <img src={storeCart} id="store-cart" />
+          </div>
+        </header>
       </>
     )
   }
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Home from './routes/Home.jsx'
 import Produtos from './routes/Produtos'
+import Sobre from './routes/Sobre.jsx'
 import Error from './routes/Error'
 
 const router = createBrowserRouter ([
@@ -11,8 +13,9 @@ const router = createBrowserRouter ([
     errorElement:<Error />,
 
     children: [
-      {path: '/', element: <Produtos />},
+      {path: '/', element: <Home />},
       {path: '/Produtos', element: <Produtos />},
+      {path: '/Sobre', element: <Sobre />}
     ]
   }
 ])
