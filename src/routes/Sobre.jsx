876021d8a3246ca30e0a74d3dd919e08f082d57a
Joraysuke <rayaraamaro@gmail.com>
2024-04-02 20:15:46 -0300

    feat: routes about added

---
+++ src/routes/Sobre.jsx
@@ -0,0 +1,28 @@
+export default function Sobre() {
+
+    return (
+      <>
+       <section id="about">
+        <div id="title-about">
+            <h1>CONHEÇA A</h1>
+        </div>
+        <div id="name-about">
+            <h1>AESTRIX</h1>
+        </div>
+        <div className="text1-about">
+            <p>Explore a AESTRIX, onde a</p>
+            <p>excelência atlética ganha vida.</p>
+            <p>Com uma cuidadosa seleção,</p>
+            <p>estamos aqui para superar </p>
+            
+        </div>
+
+
+
+
+
+        </section>
+      </>
+    )
+  }
+  
