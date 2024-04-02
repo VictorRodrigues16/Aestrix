d99ede11505452eefddd31e237d7c425e285871d
Marcelo <marcelovieira048@gmail.com>
2024-04-02 20:59:38 -0300

    feat: footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,10 @@
-
+import LinkedIn from '../assets/LinkedIn.svg'
+import Instagram from '../assets/Instagram.svg'
+import Facebook from '../assets/Facebook.svg'
+import Whatsapp from '../assets/Whatsapp.svg'
+import Email from '../assets/Email.svg'
+import Logo from '../assets/LogoAestrix.svg'
+import '../styles/Footer.scss'
 
 export default function Footer() {
 
@@ -6,15 +12,22 @@ export default function Footer() {
       <>
         <footer id='format-footer'>
           <div id='icons'>
-            
+          <img src={LinkedIn} alt="LinkeIcon" />
+          <img src={Instagram} alt="InstaIcon" />
+          <img src={Facebook} alt="FaceIcon" />
+          <img src={Whatsapp} alt="WhatsIcon" />
+          <img src={Email} alt="EmailIcon" />
           </div>
-          <div>
+          <div id='logo-aestrix'>
+            <img src={Logo} alt="LogoIcon" />
             <h1>AESTRIX</h1>
+          </div>
+          <div id='rights-reserved'>
+            <p>FIAP - Challenge 2023/2024</p>
+            <p>design by joraysuke</p>
           </div>
         </footer>
       </>
     )
   }
-  
-
   
