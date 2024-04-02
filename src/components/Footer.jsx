f8341e7beb5c0531d685d90e4d09f3a3a2ea8457
Marcelo <marcelovieira048@gmail.com>
2024-04-02 20:30:13 -0300

    feat: contact added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,11 @@ export default function Footer() {
     return (
       <>
         <footer id='format-footer'>
-          <div>
+          <div id='icons'>
             
+          </div>
+          <div>
+            <h1>AESTRIX</h1>
           </div>
         </footer>
       </>
