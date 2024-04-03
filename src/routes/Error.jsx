ea5a139df6cbbf3c554746e76de9b9e46f83956c
Marcelo <marcelovieira048@gmail.com>
2024-04-02 21:17:37 -0300

    feat: home botton on error page added

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,6 +1,8 @@
 import {} from 'react'
+import { Link } from 'react-router-dom'
 import errorMsg from '../assets/errorMsg.svg'
 import '../styles/Error.scss'
+
 
 export default function Error() {
 
@@ -10,6 +12,7 @@ export default function Error() {
             <div className='format-page'>
                 <img src={errorMsg} alt="errorMsg" />
                 <h1>404 - PAGE NOT FOUND... PLEASE TRY AGAIN!</h1>
+                <Link to='/' className='return-btn'> RETURN TO HOME </Link>
             </div>
         </section>
       </>
