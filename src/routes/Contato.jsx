85092c6c30db374736af47217570cea1fe328eb0
Felipe <felipe.salazar2004@outlook.com>
2024-04-02 21:11:17 -0300

    Feat: contact screen

--- src/routes/Contato.jsx
+++ src/routes/Contato.jsx
@@ -1,10 +1,57 @@
+import {} from "react";
+import "../styles/Contato.scss";
 
 export default function Contato() {
+  return (
+    <>
+      <section className="contact">
+        <h1 className="form-title">CONTATE-NOS</h1>
 
-    return (
-      <>
+        <form className="forms">
+          <div className="form-group">
+            <input
+              id="name"
+              required=""
+              placeholder="Nome Completo*"
+              className="form-input"
+              type="text"
+            />
+          </div>
 
-      </>
-    )
-  }
-  
+          <div className="form-group">
+            <input
+              id="email"
+              required=""
+              placeholder="E-mail*"
+              className="form-input"
+              name="email"
+              type="email"
+            />
+          </div>
+
+          <div className="form-group">
+            <textarea
+              required=""
+              placeholder="Mensagem*"
+              className="form-input"
+              name="message"
+              id="message"
+            ></textarea>
+          </div>
+
+          <div id="check">
+            <input className="checkbox" type="checkbox" id="checkbox" />
+            <label id="info-check">
+              Concordo com o envio dos dados e autorizo o ICR Conecta a entrar
+              contato comigo através deles.
+            </label>
+          </div>
+
+          <button className="form-button" type="submit">
+            ENVIAR MENSAGEM
+          </button>
+        </form>
+      </section>
+    </>
+  );
+}
