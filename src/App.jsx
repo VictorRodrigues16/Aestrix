eee2699f0cd988457db194d5af9e50c2d3c03f0c
Joraysuke <rayaraamaro@gmail.com>
2024-04-02 21:46:06 -0300

    feat: added style about

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {} from 'react'
 import Footer from "./components/Footer"
 import Nav from "./components/Nav"
 import { Outlet } from 'react-router-dom'
-import Sobre from './routes/Sobre'
+
 
 
 function App() {
--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -1,4 +1,5 @@
 import '../styles/Sobre.scss'
+import imagem from '../assets/img-about.svg'
 
 export default function Sobre() {
 
@@ -6,35 +7,37 @@ export default function Sobre() {
       <>
        <section id="about">
 
-        <div id="title-about">
-            <h1>CONHEÇA A</h1>
+        <aside className='left-content'>
+            <div id="title-about">
+                <h1>CONHEÇA A</h1>
+            </div>
+
+            <div id="name-about">
+                <h1>AESTRIX</h1>
+            </div>
+
+            <div className="text1-about">
+                <p>
+                    Explore a <span id='bold-about'>AESTRIX</span>, 
+                    onde a excelência atlética ganha vida. 
+                    Com uma cuidadosa seleção, 
+                    estamos aqui para superar desafios e alcançar suas metas.
+                </p>
+            </div>
+
+            <div className='text2-about'>
+                <p>
+                Junte-se a nós para explorar 
+                novos horizontes e criar 
+                memórias inesquecíveis.
+                </p>
+            </div>
+        </aside>
+        
+
+        <div id='imagem-about'>
+            <img src={imagem} alt="imagem"/>
         </div>
-
-        <div id="name-about">
-            <h1>AESTRIX</h1>
-        </div>
-
-        <div className="text1-about">
-            <p>
-                Explore a <span id='bold-about'>AESTRIX</span>, 
-                onde a excelência atlética ganha vida. 
-                Com uma cuidadosa seleção, 
-                estamos aqui para superar desafios e alcançar suas metas.
-            </p>
-        </div>
-
-        <div className='text2-about'>
-            <p>
-            Junte-se a nós para explorar 
-            novos horizontes e criar 
-            memórias inesquecíveis.
-            </p>
-        </div>
-
-
-
-
-
 
         </section>
       </>
