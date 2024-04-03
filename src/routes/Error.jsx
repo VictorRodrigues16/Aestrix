ff25ddb70cadfcbb6692fe0b005e4699ff0c0c5b
Felipe <felipe.salazar2004@outlook.com>
2024-04-02 21:23:52 -0300

    Fix: error page

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,21 +1,19 @@
-import {} from 'react'
-import { Link } from 'react-router-dom'
-import errorMsg from '../assets/errorMsg.svg'
-import '../styles/Error.scss'
-
+import {} from "react";
+import { Link } from "react-router-dom";
+import errorMsg from "../assets/errorMsg.svg";
+import "../styles/Error.scss";
 
 export default function Error() {
-
-    return (
-      <>
-        <section id="error-page">
-            <div className='format-page'>
-                <img src={errorMsg} alt="errorMsg" />
-                <h1>404 - PAGE NOT FOUND... PLEASE TRY AGAIN!</h1>
-                <Link to='/' className='return-btn'> RETURN TO HOME </Link>
-            </div>
-        </section>
-      </>
-    )
-  }
-  
+  return (
+    <>
+      <section className="format-page">
+        <img src={errorMsg} alt="errorMsg" />
+        <h1>404 - PAGE NOT FOUND... PLEASE TRY AGAIN!</h1>
+        <Link to="/" className="return-btn">
+          {" "}
+          RETURN TO HOME{" "}
+        </Link>
+      </section>
+    </>
+  );
+}
